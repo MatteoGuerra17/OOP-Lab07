e8ba5b72a36b8b98df086e18abeeb8804f0859b6
Matteo Guerra <matteo.guerra15@studio.unibo.it>
2020-11-10 11:58:18 +0100

    Completed es.enum1

--- src/it/unibo/oop/lab/enum1/SportSocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/enum1/SportSocialNetworkUserImpl.java
@@ -3,6 +3,9 @@
  */
 package it.unibo.oop.lab.enum1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUserImpl;
 import it.unibo.oop.lab.socialnetwork.User;
 
@@ -30,7 +33,7 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
      * 
      * add a field to keep track of the set of sports followed/done by a user
      */
-
+	public List<Sport> sport = new ArrayList<>();
     /**
      * Builds a new {@link SportSocialNetworkUserImpl}.
      * 
@@ -78,7 +81,9 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
      */
     // TODO
     public void addSport(final Sport sport) {
-
+    	if(!this.sport.contains(sport)) {
+    		this.sport.add(sport);
+    	}
     }
 
     /**
@@ -90,6 +95,6 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
      * @return true if the user likes sport s
      */
     public boolean hasSport(final Sport s) {
-        return false;
+        return this.sport.contains(s);
     }
 }
